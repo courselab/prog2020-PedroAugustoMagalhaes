c44259a448dd2656a7fd97cab5dcaf43da6ed517
PedroAug <pedroaugusto@usp.br>
2020-06-23 11:21:47 +0000

    teste

--- m001.c
+++ m001.c
@@ -25,20 +25,17 @@
 
 int greatest (int a, int b, int c)
 {
-  if(a>b && a>c)
+  int maior;
+  maior = a;
+  if(b>=maior)
   {
-    return a;
+    maior = b;
   }
-  else
-    if(b>a && b>c)
-    {
-      return b;
-    }
-    else
-      if(c>a && c>b)
-      {
-        return c;
-      }
+  if (c>= maior)
+  {
+    maior = c;
+  }
+  return maior;
 }
 
 /* Do not edit this function. */
